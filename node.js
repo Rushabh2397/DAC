71736b8aa84c29d8193a9b28d4c0cacdb7bdfbc3
Rushabh <rushabhsancheti2397@gmail.com>
2019-11-26 15:26:03 +0530

    node.js

--- node.js
+++ node.js
@@ -1,3 +1,3 @@
 console.log("Hi Rushabh Welcome to Git");
 console.log("You made changes for first time");
-console.log("You have madre changes second time");
+console.log("You have made changes second time");
